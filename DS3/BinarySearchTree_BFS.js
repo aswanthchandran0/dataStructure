7b48275f324e168206287b45fdd5220bb2fed2da
aswanth <aswanthc26@gmail.com>
2024-01-31 10:20:59 +0530

    binary search Tree implemented

---
+++ DS3/BinarySearchTree_BFS.js
@@ -0,0 +1,117 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null
+    }
+    isEmpty(){
+        return this.root == null
+    }
+    insert(value){
+        const newNode = new Node(value)
+        if(this.isEmpty()){
+            this.root = newNode
+        }else{
+            this.insertNode(this.root,newNode)
+        }
+    }
+    insertNode(root,newNode){
+        if(newNode.value < root.value){
+            if(root.left == null){
+                root.left = newNode
+            }else{
+                this.insertNode(root.left,newNode)
+            }
+        }else{
+            if(root.right == null){
+                root.right = newNode
+            }else{
+                this.insertNode(root.right,newNode)
+            }
+        }
+    }
+
+    search(root,value){
+        if(!root){
+            return false
+        }else{
+            if(value == root.value){
+                return true
+            }else if(value < root.value){
+              return   this.search(root.left)
+            }else{
+                return this.search(root.right)
+            }
+        }
+    }
+
+    preOrder(root){
+        if(root){
+            console.log(root.value)
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+
+    }
+    inOrder(root){
+        if(root){
+            this.inOrder(root.left)
+            console.log(root.value);
+            this.inOrder(root.right)
+        }
+    }
+    postOrder(root){
+        if(root){
+            this.postOrder(root.left)
+            this.postOrder(root.right)
+            console.log(root.value);
+        }
+    }
+    levelOrder(){
+        let queue = []
+        queue.push(this.root)
+        while(queue.length){
+           let curr = queue.shift()
+           console.log(curr.value);
+           if(curr.left){
+            queue.push(curr.left)
+           }
+           if (curr.right){
+            queue.push(curr.right)
+           }
+        }
+    }
+    min(root){
+        if(!root.left){
+            return root.value
+        }else{
+          return  this.min(root.left)
+        }
+    }
+    max(root){
+        if(!root.right){
+            return root.value
+        }else{
+        return  this.max(root.right)
+        }
+    }
+}
+
+const bst = new BinarySearchTree()
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+bst.insert(3)
+bst.insert(7)
+bst.levelOrder()
+
+console.log('min',bst.min(bst.root));
+console.log('max',bst.max(bst.root));
--- DS3/binarySearchTree.js
+++ DS3/binarySearchTree.js
@@ -5,3 +5,43 @@ constructor(value){
     this.right = null
 }
 }
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null
+    }
+    isEmpty(){
+        return this.root === null
+    }
+    insert(value){
+        const newNode = new Node(value)
+        if(this.isEmpty()){
+            this.root = newNode
+        }else{
+            this.insertNode(this.root,newNode)
+        }
+    }
+    insertNode(root,newNode){
+        if(newNode.value < root.value){
+            if(root.left ==null){
+                root.left = newNode
+            }else{
+                this.insertNode(root.left,newNode)
+            }
+        }else{
+            if(root.right ==null){
+                root.right = newNode
+            }else{
+                this.insertNode(root.right,newNode)
+            }
+        }
+    }
+}
+
+const bst = new BinarySearchTree()
+console.log('Tree is empty?',bst.isEmpty());
+bst.insert(10)
+bst.insert(10)
+bst.insert(10)
+bst.insert(40)
+console.log('Tree is empty?',bst.isEmpty());
